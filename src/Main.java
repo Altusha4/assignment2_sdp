a3ab9a57cb6fc12814d6d7fcc8928e15a966cd10
Altusha <240782@astanait.edu.kz>
2025-09-17 19:33:00 +0600

    add call and have output

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Logistics RoadLogistics = new RoadLogistics();
         Transport Truck = RoadLogistics.createTransport();
+        Truck.deliver();
     }
 }
